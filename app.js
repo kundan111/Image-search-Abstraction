d41f281f0ce5796d0ebba357ff62cd4832047b18
kundan111 <kundanranjan789@gmail.com>
2017-06-23 23:16:45 +0530

    Added a file searchTerm.js and made chages to app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var bodyParser = require("body-parser");
 var cors = require("cors");
 var mongoose = require("mongoose");
 var bing = require("node-bing-api")({accKey: "f2d63d8d6fd249d5ae7ab02b9092daec"});
+var searchTerm = require("./models/searchTerm");
+// mongoose.Promise = global.Promise;
+
 
 
 // instanting the express
@@ -13,23 +16,71 @@ var app = express();
 //using app
 app.use(bodyParser.json());
 app.use(cors());
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/searchTerms");
+
+//get all search terms from database
+
+app.get("/api/recentsearches",function(req,res,next){
+
+    searchTerm.find({},function(err,data){
+
+        res.json(data);
+
+    });
+
+});
 
 //making a get call
-app.get("/api/imagesearch/:searchval*",function(req,res,next){
+app.get("/api/imagesearch/:searchVal*",function(req,res,next){
 
-    var searchval = req.params.searchval;
+    var searchVal = req.params.searchVal;
     var offset = req.query.offset;
 
-  return res.json(
-  {  searchval,
-    offset
-  })
+
+var data = new searchTerm({
+
+  searchVal,
+  searchDate : new Date()
+
+});
+
+data.save(function(err){
+    if(err){
+        return res.send("Oooops!..Error");
+    }
 
 
 });
 
+bing.images(searchVal,{
+
+  top: 10
+
+},function(error,rez,body){
 
 
+  // res.json(body);
+
+
+    var bingData = [];
+
+    for(var i =0 ;i <10 ;i++){
+
+        bingData.push({
+
+            url: body.value[i].webSearchUrl,
+            snippet: body.value[i].name,
+            thumbnail: body.value[i].thumbnailUrl,
+            context: body.value[i].hostPageDisplayUrl
+
+        });
+
+}
+res.json(bingData);
+
+});
+
+});
 
 
 
---
+++ models/searchTerm.js
@@ -0,0 +1,24 @@
+//require mongooose and schrma
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+//model
+
+var searchTermSchema = new Schema(
+  {
+      searchVal: String,
+      searchDate: Date
+
+  },
+  {
+    timestamps: true
+
+  }
+);
+
+//connect model and collection
+
+var ModelClass = mongoose.model('searchTerm', searchTermSchema);
+
+module.exports = ModelClass;
